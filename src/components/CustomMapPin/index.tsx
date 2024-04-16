752c10a831a4509d7bf087caadd87e70ac436bb5
Drejky <peter.kudela9@gmail.com>
2024-04-16 15:20:31 +0200

    Fix contrafactual example name and map pin graph

--- src/components/CustomMapPin/index.tsx
+++ src/components/CustomMapPin/index.tsx
@@ -39,26 +39,22 @@ const CustomMapPin: React.FC<CustomMapPinProps> = ({ name, classname }) => {
   });
 
   const data = {
-    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    labels: ['Vytazenost', 'Nastup', 'Vystup', 'Cas'],
     datasets: [
       {
-        label: '# of Votes',
-        data: [12, 19, 3, 5, 2, 3],
+        label: 'Feature Attribute',
+        data: [95, 90, 80, 30],
         backgroundColor: [
           'rgba(255, 99, 132, 0.2)',
           'rgba(54, 162, 235, 0.2)',
           'rgba(255, 206, 86, 0.2)',
           'rgba(75, 192, 192, 0.2)',
-          'rgba(153, 102, 255, 0.2)',
-          'rgba(255, 159, 64, 0.2)',
         ],
         borderColor: [
           'rgba(255, 99, 132, 1)',
           'rgba(54, 162, 235, 1)',
           'rgba(255, 206, 86, 1)',
           'rgba(75, 192, 192, 1)',
-          'rgba(153, 102, 255, 1)',
-          'rgba(255, 159, 64, 1)',
         ],
         borderWidth: 1,
       },
@@ -67,17 +63,6 @@ const CustomMapPin: React.FC<CustomMapPinProps> = ({ name, classname }) => {
 
   const options = {
     indexAxis: 'y',
-    elements: {
-      bar: {
-        borderWidth: 2,
-      },
-    },
-    responsive: true,
-    plugins: {
-      legend: {
-        position: 'top',
-      },
-    },
   };
   return (
     <div className={classNames(classname, styles.container)}>
